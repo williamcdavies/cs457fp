0bc289045e6a9cfc3f4b60370bb928716aca01b2
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-11-22 16:33:43 -0800

    Create log.py

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,5 @@
 from fastapi        import FastAPI
-from server.routers import fire_area, hms_fire, hms_smoke, lake, lake_buffer, lake_point, lake_poly, populated_place
+from server.routers import fire_area, hms_fire, hms_smoke, lake, lake_buffer, lake_point, lake_poly, log, populated_place
 
 
 app = FastAPI()
@@ -12,4 +12,5 @@ app.include_router(lake.router)
 app.include_router(lake_buffer.router)
 app.include_router(lake_point.router)
 app.include_router(lake_poly.router)
+app.include_router(log.router)
 app.include_router(populated_place.router)
---
+++ server/routers/log.py
@@ -0,0 +1,29 @@
+from fastapi           import APIRouter, Depends, HTTPException, Query
+from sqlmodel          import Session, select
+from server.db.session import get_session
+from server.models.log import Log
+from typing            import Annotated
+
+
+router = APIRouter(prefix="/logs", tags=["logs"])
+
+
+@router.get("/", response_model=list[Log])
+def read_logs(
+    session: Annotated[Session, Depends(get_session)],
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100
+):
+    logs = session.exec(select(Log).order_by(Log.id).offset(offset).limit(limit)).all()
+    return logs
+
+
+@router.get("/{id}", response_model=Log)
+def read_lake(
+    id: int, 
+    session: Annotated[Session, Depends(get_session)]
+):
+    log = session.get(Log, id)
+    if not log:
+        raise HTTPException(status_code=404, detail="Object not found")
+    return log
