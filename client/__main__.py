5096a9472d17c0a51a4921015e3ba143003251e7
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-11-22 18:57:56 -0800

    Update __main__.py

--- client/__main__.py
+++ client/__main__.py
@@ -1,4 +1,5 @@
 from fire_area import get_fire_areas, get_fire_area
+from hms_fire  import get_hms_fires, get_hms_fire
 
 def main():
     while True:
@@ -7,7 +8,9 @@ def main():
 usage: 
     \\q
     \\get_fire_areas [offset] [limit]
-    \\get_fire_area  [year] [id]
+    \\get_fire_area  [year]   [id]
+    \\get_hms_fires  [offset] [limit]
+    \\get_hms_fire   [year]   [id]
 """
         )
         cmd = input("> ").strip().split()
@@ -22,6 +25,10 @@ usage:
                 get_fire_areas(cmd[1], cmd[2])
             elif cmd[0] == "\\get_fire_area":
                 get_fire_area(cmd[1], cmd[2])
+            elif cmd[0] == "\\get_hms_fires":
+                get_hms_fires(cmd[1], cmd[2])
+            elif cmd[0] == "\\get_hms_fire":
+                get_hms_fire(cmd[1], cmd[2])
             else:
                 print(f"command not found: {cmd[0]}")
         except IndexError:
